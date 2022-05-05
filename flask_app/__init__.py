cb8d42320415005fe1baacd0a84a146fa19209ef
MSBivens <mikebiv95@gmail.com>
2022-05-04 21:29:44 -0500

    project update

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,4 +1,9 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("home.html")
+
 app.secret_key = "Get Ready to Rumble!"
