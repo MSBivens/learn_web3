f3eafd7275c98fc28cc1668adceebd25470d83f6
MSBivens <mikebiv95@gmail.com>
2022-05-07 11:47:28 -0500

    built admins controller

---
+++ flask_app/controllers/admins.py
@@ -0,0 +1,52 @@
+from flask import render_template,redirect,session,request, flash
+from flask_app import app
+from flask_app.models.admin import Admin
+from flask_app.models.suggestion import Suggestion
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/register',methods=['POST'])
+def register():
+
+    if not Admin.validate_register(request.form):
+        return redirect('/')
+    data ={ 
+        "username": request.form['username'],
+        "email": request.form['email'],
+        "password": bcrypt.generate_password_hash(request.form['password'])
+    }
+    id = Admin.save(data)
+    session['admin_id'] = id
+
+    return redirect('/dashboard')
+
+@app.route('/login',methods=['POST'])
+def login():
+    admin = Admin.get_by_email(request.form)
+
+    if not admin:
+        flash("Invalid Email","login")
+        return redirect('/')
+    if not bcrypt.check_password_hash(admin.password, request.form['password']):
+        flash("Invalid Password","login")
+        return redirect('/')
+    session['admin_id'] = admin.id
+    return redirect('/dashboard')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'admin_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['admin_id']
+    }
+    return render_template("dashboard.html",admin=Admin.get_by_id(data),suggestions=Suggestion.get_all())
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
