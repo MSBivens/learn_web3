1af09379dab1ce62dc5f798e136445c493026cab
MSBivens <mikebiv95@gmail.com>
2022-05-08 17:15:45 -0500

    created client side routes

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -5,4 +5,24 @@ app = Flask(__name__)
 def index():
     return render_template("home.html")
 
+@app.route('/podcasts')
+def podcasts():
+    return render_template("podcasts.html")
+
+@app.route('/newsletters')
+def newsletters():
+    return render_template("newsletters.html")
+
+@app.route('/discords')
+def discords():
+    return render_template("discords.html")
+
+@app.route('/twitters')
+def twitters():
+    return render_template("twitters.html")
+
+@app.route('/4dev')
+def dev():
+    return render_template("4dev.html")
+
 app.secret_key = "Get Ready to Rumble!"
